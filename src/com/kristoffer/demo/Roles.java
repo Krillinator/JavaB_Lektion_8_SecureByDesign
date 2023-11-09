02d5472a49d1a9d257b82ee074e1902052f4f698
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-11-09 12:35:59 +0100

    Initial Commit

---
+++ src/com/kristoffer/demo/Roles.java
@@ -0,0 +1,15 @@
+package com.kristoffer.demo;
+
+public enum Roles {
+
+    WARRIOR("I am STRONK", 15),
+    MAGE("I am SMORT", 15);
+
+    public final String description;
+    public final int health;
+
+    Roles(String description, int health) {
+        this.description = description;
+        this.health = health;
+    }
+}
